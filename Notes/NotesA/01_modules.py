19e40838f39372d568f3cf128b08cac8e06ab2ff
juliamarks <jmarks21@fwparker.org>
2020-01-28 14:23:03 -0600

    github

---
+++ Notes/NotesA/01_modules.py
@@ -0,0 +1,19 @@
+# MODULES, IMPORTS, LIBRARIES
+import turtle
+from math import pi, cos  # import single items from a lib
+from random import *  # imports everything locally
+import random as r  # use your own keyword
+import import_me
+
+
+
+if __name__ == "__main__":
+    #this code only runs when you run THIS FILE FIRST
+    print("Hello")
+    # my_turtle = turtle.Turtle()
+    print(pi)
+    print(cos(0.5))
+    # print(sin(0.5))
+    print(randrange(1, 7))
+    print(r.random())
+    print(import_me.x)
